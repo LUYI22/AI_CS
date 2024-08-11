b2bfd4af05c1b9765149948aed57b221758e95ba
Anna Lu <anna.lu778899@gmail.com>
2024-08-11 16:56:25 -0400

    Chat box size

--- app/page.js
+++ app/page.js
@@ -86,8 +86,8 @@ export default function Home() {
     >
       <Stack
         direction={"column"}
-        width="500px"
-        height="700px"
+        width="600px"
+        height="800px"
         border="1px solid black"
         p={2}
         spacing={3}
